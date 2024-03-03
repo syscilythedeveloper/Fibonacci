ccd9a13eb693f0396280b522cdbe6b75c295a6f9
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-03-02 19:18:57 -0500

    removing redundant code

--- FibonacciSequence.js
+++ FibonacciSequence.js
@@ -1,9 +1,7 @@
 function fibonacci(n) {
   output = [];
   for (var i = 0; i < n; i++) {
-    if (i == 0) {
-      output.push(i);
-    } else if (i == 1) {
+    if (i <= 1) {
       output.push(i);
     } else {
       lastTwo = output.slice(-2);
@@ -16,5 +14,4 @@ function fibonacci(n) {
   return output;
 }
 
-fibOf20 = fibonacci(20);
-console.log(fibOf20);
+console.log(fibonacci(20));
