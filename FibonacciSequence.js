5d6da0cad8b558e1ed40788a99b26a7ecdf3602c
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-03-07 14:43:52 -0500

    show first and last index

--- FibonacciSequence.js
+++ FibonacciSequence.js
@@ -5,12 +5,14 @@ function fibonacci(n) {
       output.push(i);
     } else {
       lastTwo = output.slice(-2);
-      var value1 = lastTwo[0];
-      var value2 = lastTwo[1];
-      var newNumb = value1 + value2;
-      output.push(newNumb);
+      // var value1 = lastTwo[0];
+      // var value2 = lastTwo[1];
+      // var newNumb = value1 + value2;
+      output.push(lastTwo[0] + lastTwo[1]);
     }
   }
+  console.log("last value of output is: " + output[output.length - 1]);
+  console.log("first value of output is: " + output[0]);
   return output;
 }
 
